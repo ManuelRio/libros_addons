7d02ce9ee5c0212fecd0997f789a7d8990bd11df
ManuelRio <86972089+ManuelRio@users.noreply.github.com>
2021-07-05 17:33:52 -0400

    Subir la Fase 1

---
+++ libros_addons/libros_registro/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Libreria",
+    "description": "Aplicacion que permite buscar libros por su titulo y autor",
+    "data":[
+        'views/views.xml',
+        'security/security.xml'
+    ]
+}
---
+++ libros_addons/libros_registro/models/models.py
@@ -0,0 +1,20 @@
+from odoo import models,fields 
+
+class Data(models.Model):
+    _name = "library.data"
+
+    name = fields.Char(string="Book's name")
+    author_id = fields.Many2one('library.author', string="Author's name")
+    date_e = fields.Integer(string="Realese date") 
+    quan_e = fields.Integer( string="Quantity in stock")
+    condition = fields.Selection(selection = [("available", "Available"), ("not available", "Not available")], string="Condition")
+    belongs= fields.Boolean(string="It belongs to a collection?")
+    series = fields.Char(string="Collection's name" )
+
+class Authors(models.Model):
+    _name = "library.author"
+
+    name = fields.Char(string="Author's name" )
+    books_ids = fields.One2many('library.data', inverse_name = 'author_id', string="Books")
+
+
