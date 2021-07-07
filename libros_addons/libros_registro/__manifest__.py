870a8de18db73a881736ac725c61ccb3286374c9
ManuelRio <86972089+ManuelRio@users.noreply.github.com>
2021-07-06 20:19:22 -0400

    subiendo la fase I

--- libros_addons/libros_registro/__manifest__.py
+++ libros_addons/libros_registro/__manifest__.py
@@ -2,7 +2,8 @@
     "name": "Libreria",
     "description": "Aplicacion que permite buscar libros por su titulo y autor",
     "data":[
-        'views/views.xml',
+        'views/views_books.xml',
+        'views/views_authors.xml',
         'security/security.xml'
     ]
 }
--- libros_addons/libros_registro/models/models.py
+++ libros_addons/libros_registro/models/models.py
@@ -1,20 +1,20 @@
 from odoo import models,fields 
 
 class Data(models.Model):
-    _name = "library.data"
+    _name="library.data"
 
-    name = fields.Char(string="Book's name")
-    author_id = fields.Many2one('library.author', string="Author's name")
-    date_e = fields.Integer(string="Realese date") 
-    quan_e = fields.Integer( string="Quantity in stock")
-    condition = fields.Selection(selection = [("available", "Available"), ("not available", "Not available")], string="Condition")
-    belongs= fields.Boolean(string="It belongs to a collection?")
-    series = fields.Char(string="Collection's name" )
+    name=fields.Char(string="Book's name")
+    author_id=fields.Many2one('library.author', string="Author's name")
+    date_e=fields.Date(string="Realese date") 
+    quan_e=fields.Integer( string="Quantity in stock")
+    state=fields.Selection(selection = [("available", "Available"), ("not available", "Not available")], string="State")
+    belongs=fields.Boolean(string="It belongs to a collection?")
+    series =fields.Char(string="Collection's name" )
 
 class Authors(models.Model):
-    _name = "library.author"
+    _name="library.author"
 
-    name = fields.Char(string="Author's name" )
-    books_ids = fields.One2many('library.data', inverse_name = 'author_id', string="Books")
+    name=fields.Char(string="Author's name" )
+    books_ids=fields.One2many('library.data', inverse_name = 'author_id', string="Books")
 
 
